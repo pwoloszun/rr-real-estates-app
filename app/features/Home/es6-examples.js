60e8da7b5ebe934f314bde063132264beaed967a
Piotr Woloszun <piotr.woloszun@gmail.com>
2017-03-13 07:30:49 +0100

    es6 syntax

---
+++ app/features/Home/es6-examples.js
@@ -0,0 +1,13 @@
+// TODO: import/export
+
+// TODO: fat arrow function
+
+// TODO: string interpolation
+
+// TODO: property shorthand
+
+// TODO: destructuring Object, Array, func arguments, default values
+
+// TODO: classes, inheritance, base class access
+
+

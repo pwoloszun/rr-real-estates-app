abcb8f5bbe894d024b140381f620ecca348d2352
Piotr Woloszun <piotr.woloszun@gmail.com>
2017-03-13 07:35:51 +0100

    minor changes

--- app/features/Home/es6-examples.js
+++ app/features/Home/es6-examples.js
@@ -3,11 +3,29 @@
 // TODO: fat arrow function
 
 // TODO: string interpolation
+function stringInterpolationExample() {
+
+}
 
 // TODO: property shorthand
+function propertyShorthandExample() {
+
+}
 
 // TODO: destructuring Object, Array, func arguments, default values
+function destructuringExample() {
+
+}
 
 // TODO: classes, inheritance, base class access
+class User {
+  constructor(login, password) {
+    this.login = login;
+    this.password = password;
+  }
 
+  getLogin() {
+    return this.login;
+  }
+}
 

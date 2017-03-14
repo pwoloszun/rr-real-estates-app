38627a89e5529d7031d2cb7eb18ab01e365db510
Piotr Woloszun <piotr.woloszun@gmail.com>
2017-03-14 19:44:00 +0100

    minor changes

--- app/features/About/components/HelloWorld/index.js
+++ app/features/About/components/HelloWorld/index.js
@@ -4,35 +4,14 @@ import styles from "./styles.css";
 
 export default class HelloWorld extends Component {
 
-  static propTypes = {
-    user: React.PropTypes.objectOf(React.PropTypes.string),
-    onHeaderClick: React.PropTypes.func
-  };
-
-  constructor(props) {
-    super(props);
-    this.name = `HW ${Math.random()}`;
-    this.handleHeaderClick = ::this.handleHeaderClick;
-  }
-
   render() {
-    let content = null;
-    const {user} = this.props;
-    if (user) {
-      content = (<p>Greetings {user.name.toUpperCase()}!</p>);
-    }
     return (
       <div>
-        <h3 onClick={this.handleHeaderClick} className={styles.helloWorld}>Hi!!!</h3>
-        {content}
+        <h3 className={styles.helloWorld}>
+          Hi!!!
+        </h3>
       </div>
     );
   }
 
-  handleHeaderClick(event) {
-    event.preventDefault();
-    console.log("qq", this.name);
-    const {onHeaderClick, user} = this.props;
-    onHeaderClick(user);
-  }
 }
